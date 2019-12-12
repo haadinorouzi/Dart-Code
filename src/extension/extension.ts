65da24b82adc347bd30bc706d082060fd756fbbf
Danny Tuppeny <danny@tuppeny.com>
2019-12-12 12:05:52 +0000

    Refactor fileTracker to better support DAS + LSP

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -200,6 +200,7 @@ export async function activate(context: vs.ExtensionContext, isRestart: boolean 
 	const analyzerStartTime = new Date();
 
 	analyzer = isUsingLsp ? new LspAnalyzer(logger, sdks, dartCapabilities) : new DasAnalyzer(logger, analytics, sdks, dartCapabilities, workspaceContext);
+	const lspAnalyzer = isUsingLsp ? (analyzer as LspAnalyzer) : undefined;
 	const dasAnalyzer = isUsingLsp ? undefined : (analyzer as DasAnalyzer);
 	const dasClient = dasAnalyzer ? dasAnalyzer.client : undefined;
 	const lspClient = dasClient ? undefined : (analyzer as LspAnalyzer).client;
@@ -587,10 +588,10 @@ export async function activate(context: vs.ExtensionContext, isRestart: boolean 
 			currentAnalysis: () => analyzer.onCurrentAnalysisComplete,
 			daemonCapabilities: flutterDaemon ? flutterDaemon.capabilities : DaemonCapabilities.empty,
 			dartCapabilities,
+			dasFileTracker: dasAnalyzer ? dasAnalyzer.fileTracker : undefined,
 			debugCommands,
 			debugProvider,
 			envUtils,
-			fileTracker: dasAnalyzer ? dasAnalyzer.fileTracker : undefined,
 			flutterCapabilities,
 			flutterOutlineTreeProvider,
 			get cursorIsInTest() { return cursorIsInTest; },
